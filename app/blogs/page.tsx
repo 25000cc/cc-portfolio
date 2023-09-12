0cd873d2c7d5f119caeb5250529406293a5be4ee
25000cc <25000ccprogram@gmail.com>
2023-09-12 18:40:06 +0900

    Experimenting with microCMS

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import Link from 'next/link';
+import { getList } from '../../libs/microcms';
+import { client } from '../../libs/client';
+
+export default async function Blogs() {
+  const { contents } = await getList()
+  // const { contents } = await client.get({
+  //   endpoint: 'blog',
+  // })
+
+  if (!contents || contents.length === 0) {
+    return <h1>No contents</h1>;
+  }
+
   return (
     <>
-      <h1>Blogs</h1>
+      <h1>{contents.length}</h1>
+      <ul>
+        {contents.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   )
 }
---
+++ libs/microcms.ts
@@ -0,0 +1,52 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+ MicroCMSQueries,
+ MicroCMSDate,
+} from "microcms-js-sdk";
+import { client } from './client'
+
+const delay = 1000;
+
+//ブログの型定義
+export type Blog = {
+ id: string;
+ title: string;
+ content: string;
+} & MicroCMSDate;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+ throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+ throw new Error("MICROCMS_API_KEY is required");
+}
+
+// ブログ一覧を取得
+export const getList = async () => {
+ const listData = await client.getList<Blog>({
+  endpoint: "blog",
+ });
+
+ // データの取得が目視しやすいよう明示的に遅延効果を追加
+ await new Promise((resolve) => setTimeout(resolve, delay));
+
+ return listData;
+};
+
+// ブログの詳細を取得
+export const getDetail = async (
+ contentId: string,
+ queries?: MicroCMSQueries
+) => {
+ const detailData = await client.getListDetail<Blog>({
+  endpoint: "blog",
+  contentId,
+  queries,
+ });
+
+ // データの取得が目視しやすいよう明示的に遅延効果を追加
+ await new Promise((resolve) => setTimeout(resolve, delay));
+
+ return detailData;
+};
